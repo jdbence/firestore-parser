2cdf24f6d7f0c7474ff3398a1c20ac95292960d4
Joshua Bence <joshua.bence@gmail.com>
2018-03-11 17:56:12 +0000

    chore: reduce complexity

--- index.js
+++ index.js
@@ -5,22 +5,20 @@ const getFireStoreProp = (value) => {
 
 export const FireStoreParser = (value) => {
   const prop = getFireStoreProp(value)
-  if (prop) {
-    if (prop === 'integerValue') {
-      value = Number(value[prop])
-    }
-    else if (prop === 'arrayValue') {
-      value = value[prop].values.map(v => FireStoreParser(v))
-    }
-    else if (prop === 'mapValue') {
-      value = FireStoreParser(value[prop].fields)
-    }
-    else if (prop === 'geoPointValue') {
-      value = { latitude: 0, longitude: 0, ...value[prop] }
-    }
-    else {
-      value = value[prop]
-    }
+  if (prop === 'integerValue') {
+    value = Number(value[prop])
+  }
+  else if (prop === 'arrayValue') {
+    value = value[prop].values.map(v => FireStoreParser(v))
+  }
+  else if (prop === 'mapValue') {
+    value = FireStoreParser(value[prop].fields)
+  }
+  else if (prop === 'geoPointValue') {
+    value = { latitude: 0, longitude: 0, ...value[prop] }
+  }
+  else if (prop) {
+    value = value[prop]
   }
   else if (typeof value === 'object') {
     Object.keys(value).forEach(k => value[k] = FireStoreParser(value[k]))
