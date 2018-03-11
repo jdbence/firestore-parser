41bb383cc65948a0b2a48e8e077a38d5137f1849
Joshua Bence <joshua.bence@gmail.com>
2018-03-11 12:17:04 +0000

    fix: spacing

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
 const getFireStoreProp = (value) => {
-  const props = ['referenceValue', 'nullValue', 'booleanValue', 'stringValue', 'timestampValue','arrayValue', 'mapValue', 'geoPointValue', 'integerValue']
+  const props = ['referenceValue', 'nullValue', 'booleanValue', 'stringValue', 'timestampValue', 'arrayValue', 'mapValue', 'geoPointValue', 'integerValue']
   let k = Object.keys(value)[0]
   return props.indexOf(k) !== -1 ? k : null
 }
 
 export const FireStoreParser = (value) => {
   const type = typeof value
-  if(value === null || type === 'number' || type === 'string'){
+  if (value === null || type === 'number' || type === 'string') {
     return value
   }
   const prop = getFireStoreProp(value)
-  if(prop){
-  	if(prop === 'arrayValue'){
-    	value = value[prop].values
-    }else if(prop === 'mapValue'){
-    	value = value[prop].fields
-    }else if(prop === 'geoPointValue'){
-    	return {latitude:0, longitude:0, ...value[prop]}
-    } else {
-    	return value[prop]
+  if (prop) {
+    if (prop === 'arrayValue') {
+      value = value[prop].values
+    }
+    else if (prop === 'mapValue') {
+      value = value[prop].fields
+    }
+    else if (prop === 'geoPointValue') {
+      return { latitude: 0, longitude: 0, ...value[prop] }
+    }
+    else {
+      return value[prop]
     }
   }
-  
+
   const obj = {};
   Object.keys(value).forEach(k => {
     obj[k] = FireStoreParser(value[k])
